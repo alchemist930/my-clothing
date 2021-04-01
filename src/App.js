a0a8b9229b2635a7a92334572479d57bb66bc40e
alchemist930 <alchemist.barbs@gmail.com>
2021-04-01 12:31:13 +0800

    updating component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom'; 
+// import {BrowserRouter, Route, Link} from 'react-router-dom'; 
+import {BrowserRouter, Route} from 'react-router-dom'; 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+// const HatsPage = (props) =>{
+//   console.log(props);
+//   return (<div>hats page
+//       <div>
+//          <Link to='/'>homePage</Link>
+//       </div>
+//   </div>
+// );}
 
-const HatsPage = (props) =>{
-   
-  console.log(props);
-  return (<div>hats page
-      <div>
-         <Link to='/'>homePage</Link>
-      </div>
-  </div>
- 
-
-);}
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
    
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
+        {/* <Route path='/hats' component={HatsPage}/> */}
+        <Route path='/shop' component={ShopPage}/>
  
     </BrowserRouter>
      
